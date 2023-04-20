da6603958a4aac1b8d2a6dd95ed18666e8f51aa4
Pooja Palod <pooja93palod@gmail.com>
2023-04-20 18:20:06 +0530

    Create 340. Longest Substring with At Most K Distinct Characters.py

---
+++ 340. Longest Substring with At Most K Distinct Characters.py
@@ -0,0 +1,31 @@
+'''
+https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+Given a string s and an integer k, return the length of the longest substring of s that contains at most k distinct characters.
+
+ 
+
+Example 1:
+
+Input: s = "eceba", k = 2
+Output: 3
+Explanation: The substring is "ece" with length 3.
+Example 2:
+
+Input: s = "aa", k = 1
+Output: 2
+Explanation: The substring is "aa" with length 2.'''
+
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        l=0
+        m=defaultdict(int)
+        ans=0
+        for r in range(len(s)):
+            m[s[r]]+=1
+            while len(m)>k:
+                m[s[l]]-=1
+                if m[s[l]]==0:
+                    del m[s[l]]
+                l+=1
+            ans=max(ans,r-l+1)
+        return ans
