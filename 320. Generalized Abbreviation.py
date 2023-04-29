c65ce3042e0905d62270d82e6c253633fd4b2e6d
Pooja Palod <pooja93palod@gmail.com>
2023-04-29 20:37:45 +0530

    Create 320. Generalized Abbreviation.py

---
+++ 320. Generalized Abbreviation.py
@@ -0,0 +1,44 @@
+'''A word's generalized abbreviation can be constructed by taking any number of non-overlapping and non-adjacent substrings and replacing them with their respective lengths.
+
+For example, "abcde" can be abbreviated into:
+"a3e" ("bcd" turned into "3")
+"1bcd1" ("a" and "e" both turned into "1")
+"5" ("abcde" turned into "5")
+"abcde" (no substrings replaced)
+However, these abbreviations are invalid:
+"23" ("ab" turned into "2" and "cde" turned into "3") is invalid as the substrings chosen are adjacent.
+"22de" ("ab" turned into "2" and "bc" turned into "2") is invalid as the substring chosen overlap.
+Given a string word, return a list of all the possible generalized abbreviations of word. Return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: word = "word"
+Output: ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
+Example 2:
+
+Input: word = "a"
+Output: ["1","a"]'''
+
+class Solution:
+    def generateAbbreviations(self, word: str) -> List[str]:
+        res=[]
+        def helper(word, pos, cur, count):
+            if len(word) == pos:
+                if count:
+                    cur+=str(count)
+                # Once we reach the end, append current to the result
+                result.append(cur)
+            else:
+                # Skip current position, and increment count
+                helper(word, pos + 1, cur, count + 1)
+                if count:
+                    cur+=str(count)
+                # Include current position, and zero-out count
+                helper(word, pos + 1, cur + word[pos], 0)
+
+        result = []
+        helper(word, 0, '', 0)
+        return result
+
