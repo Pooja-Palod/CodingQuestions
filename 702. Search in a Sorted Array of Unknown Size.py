c644f5057d97d88eb0d991d665d9efb9c2762ae6
Pooja Palod <pooja93palod@gmail.com>
2023-05-27 12:28:11 +0530

    Create 702. Search in a Sorted Array of Unknown Size.py

---
+++ 702. Search in a Sorted Array of Unknown Size.py
@@ -0,0 +1,51 @@
+'''This is an interactive problem.
+
+You have a sorted array of unique elements and an unknown size. You do not have an access to the array but you can use the ArrayReader interface to access it. You can call ArrayReader.get(i) that:
+
+returns the value at the ith index (0-indexed) of the secret array (i.e., secret[i]), or
+returns 231 - 1 if the i is out of the boundary of the array.
+You are also given an integer target.
+
+Return the index k of the hidden array where secret[k] == target or return -1 otherwise.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: secret = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in secret and its index is 4.
+Example 2:
+
+Input: secret = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in secret
+'''
+
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        left=0
+        right=1
+        while reader.get(right)<target:
+            left=right
+            right<<=1
+            
+        while left<=right:
+            mid=left+((right-left)>>1)
+            num=reader.get(mid)
+            if num==target:
+                return mid
+            if num>target:
+                right=mid-1
+            else:
+                left=mid+1
+        return -1
